64a4d28288d8641788176112934b1e738b3c8603
Jonathan Quintanilla <quintanillajon@gmail.com>
2020-01-11 17:49:56 -0500

    fixing your shitty ass code

--- release_mo_1.py
+++ release_mo_1.py
@@ -13,28 +13,35 @@ MAX = 35
 
 #* MO number
 #* Drawing number & Description
-mo_list = []
-drawing_list = []
-description_list = []
-start_date_list = []
-finish_date_list = []
-qty_list = []
+# mo_list = []
+# drawing_list = []
+# description_list = []
+# start_date_list = []
+# finish_date_list = []
+# qty_list = []
+
+job_list = []
+builder = {}
+
 for row in range(4,MAX+1):
     for i in range(1,5):
         mo = str(sheet.cell(row=row, column=i).value)
         description_drawing = sheet.cell(row=row, column=i+1).value
         if "None" not in mo:
             break
-    mo_list.append(mo)
+    # mo_list.append(mo)
+    builder['mo'] = mo
     description_drawing_split = description_drawing.split(" - ")
-    drawing_list.append(description_drawing_split[0])
-    drawing_list.append("")
+    # drawing_list.append(description_drawing_split[0])
+    builder['drawing_list'] = description_drawing_split[0]
+
     try:
-        description_list.append(description_drawing_split[1])
+        # description_list.append(description_drawing_split[1])
+        builder['description_list'] = description_drawing_split[1]
     except:
-        description_list.append("")
-    print(mo)
-    print(len(description_drawing_split))
+        # description_list.append("")
+        continue
+
     #print(mo_list)
     #print(drawing_list)
     #print(description_list)
@@ -46,7 +53,8 @@ for row in range(4,MAX+1):
     start_month = str(start_date.month).zfill(2)
     start_day = str(start_date.day).zfill(2)
     new_start_date = "{}{}{}".format(start_month,start_day,start_year)
-    start_date_list.append(new_start_date)
+    # start_date_list.append(new_start_date)
+    builder['start_date'] = new_start_date
     #print(start_date_list)
 
     #* Finish date
@@ -56,31 +64,38 @@ for row in range(4,MAX+1):
     finish_month = str(finish_date.month).zfill(2)
     finish_day = str(finish_date.day).zfill(2)
     new_finish_date = "{}{}{}".format(finish_month, finish_day, finish_year)
-    finish_date_list.append(new_finish_date)
+    builder['finish_date'] = new_finish_date
+    # finish_date_list.append(new_finish_date)
     #print(finish_date_list)
 
     #* QTY
     qty = str(sheet.cell(row=row, column=11).value)
-    qty_list.append(qty)
+    # qty_list.append(qty)
+    builder['qty'] = qty
     #print(qty_list)
+    job_list.append(builder)
 
+complete_job = {JOB:job_list}
+# print(complete_job)
+a = json.dumps(complete_job,indent=2)
+print(a)
 #TODO - Save all info into a json file
 
-for i in range(0,len(mo_list)):
+# for i in range(0,len(mo_list)):
 
-    my_dict = { "0519-LR6": {
-            mo_list[i]: {
-                "Drawing Number": drawing_list[i],
-                "Description": description_list[i],
-                "QTY": qty_list[i],
-                "Start Date": start_date_list[i],
-                "Finish Date": finish_date_list[i],
-            }
-        }
-    }
-    data = json.dumps(my_dict, indent=2, sort_keys=True) # This would be the JSON file
+#     my_dict = { "0519-LR6": {
+#             mo_list[i]: {
+#                 "Drawing Number": drawing_list[i],
+#                 "Description": description_list[i],
+#                 "QTY": qty_list[i],
+#                 "Start Date": start_date_list[i],
+#                 "Finish Date": finish_date_list[i],
+#             }
+#         }
+#     }
+#     data = json.dumps(my_dict, indent=2, sort_keys=True) # This would be the JSON file
 
-    print(data)
+#     print(data)
 
 #TODO - Sort json file in alphabetical order. Or use a range to go in numerical order.
 #TODO - Use that info to release MO's
